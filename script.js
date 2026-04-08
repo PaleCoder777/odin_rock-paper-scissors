e9fe0440fb35a6a1297257df7b2d563d8ec9e91a
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-04-08 13:22:09 -0400

    Add variable, prompt to getHumanChoice()
    Added to getHumanChoice():
    
    - humanChoice variable
    - user prompt to input one of three choices, this
    value is put inside humanChoice variable
    
    console.log() used to verify that the variable
    is assigned the prompt value

--- script.js
+++ script.js
@@ -37,10 +37,16 @@ function getComputerChoice() {
 // Declare new function, getHumanChoice()
 function getHumanChoice() {
     // make new variable that will hold the player choice
+    let humanChoice;
+
     // since it is player choice, ask player what they will pick (rock || paper || scissor)
+    humanChoice = prompt(`Pick one of the following: "Rock", "Paper", Scissor"`, "");
+    console.log(humanChoice);
+
     // put player choice in the variable
     // we get back value inside the variable
 }
 
 
 console.log( getComputerChoice() );
+getHumanChoice();
