31140abaf4b39654e1c70c091da2139a78de4297
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-04-08 11:24:48 -0400

    Add pseudocode for step 2
    Step 2 involves writing the logic for getting
    the computerChoice, which will be one of these
    three values: rock || paper || scissors

---
+++ script.js
@@ -0,0 +1,9 @@
+// Foundations / JavaScript Basics / Rock, Paper, Scissors Project
+// Goal: Create a rock, paper, scissors game in the console, in which 5 rounds can be done and the score is shown.
+
+// Step 1: Write the logic to get the computer choice
+
+// Declare function that gives one of three values (rock, paper, scissors) as the computer choice
+    // make a variable that holds a randomn integer value from 0 - 1 || 0 - 100
+    // make another variable, that depending on random number, will equal rock || paper | scissors
+    // in the end, we get this choice, and this will be the computer choice
