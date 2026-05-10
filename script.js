6a56b099ed191419643d362ae925091fc6cad294
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-10 15:36:19 -0400

    Refactor the computer choice functin
    Switched from using if else statements to hard
    code which choice is selected to now selecting
    from an array of choices.
    
    The index to choose is generated by the Math
    random method and the floor method ensures we are
    within available index.

--- script.js
+++ script.js
@@ -2,32 +2,12 @@
 // Goal: Create a rock, paper, scissors game in the console,
 // in which 5 rounds can be done and the score is shown.
 
-// Step 1: Write the logic to get the computer choice
-// Declare function that gives one of three values (rock, paper, scissors) as the computer choice
-
+// Step 1: Write logic to get the computer choice
+// Changed from ifel statement to using array index, verified w/ console.log
 function getComputerChoice() {
-    // make a variable that holds a randomn integer value from 0-100
-    let randomNumber = Math.random() * 100;
-    //console.log(randomNumber);
-    
-    // we should round the random number we get
-    let roundedNumber = Math.round(randomNumber);
-    //console.log(roundedNumber);
-
-    // make another variable to hold computer choice
-    let computerChoice;
-
-    // depending on rounded, random number, we put rock || paper || scissor in the computer choice (0-33, 67-100)
-    if (roundedNumber < 34) {
-        computerChoice = "rock";
-    } else if (roundedNumber > 66) {
-        computerChoice = "scissors";
-    } else {
-        computerChoice = "paper";
-    }
-    //console.log(computerChoice);
-
-    // in the end, we get this choice, and this will be the computer choice
+    let gameChoices = ["rock", "paper", "scissors"]
+    let indexNumber = Math.floor(Math.random() * gameChoices.length);
+    let computerChoice = gameChoices[indexNumber];
     return computerChoice;
 }
 
@@ -52,66 +32,6 @@ function getHumanChoice() {
 // Step 3: Declare the players score variables
 // Your game will keep track of the players score.
 // You will write variables to keep track of the players score.
-// !!! MOVED INSIDE playGame() !!!
-
-
-
-
-// Step 4: Write the logic to play a single round
-// Your game will be played round by round. You will write a function that takes
-// the human and computer player choices as arguments, plays a single round,
-// increments the round winner’s score and logs a winner announcement.
-
-// Make variable that holds computerChoice (could use the function as the parameter)?
-let computerSelection = getComputerChoice();
-
-// Make variable that holds humanChoice (could use function as parameter)?
-let humanSelection = getHumanChoice();
-
-
-// Make new function that plays a round of 'Rock, Paper, Scissors', it needs two parameters
-    function playRound(computerChoice, humanChoice) {
-
-    // Make human choice variable case insensitive; type correct word, will work
-    humanChoice = humanChoice.toLowerCase();
-    //console.log(humanChoice);
-
-    // With the two choices, consider the rules of the game, who will win/lose? Can there be a tie?
-    if ((humanChoice === "rock" && computerChoice === "paper")
-    || (humanChoice === "paper" && computerChoice === "scissors")
-    || (humanChoice === "scissors" && computerChoice === "rock") ){ //all cases where computer wins
-        computerScore = ++computerScore;
-        let result = (`You lose, ${computerChoice} beats ${humanChoice}! Computer gets point!`);
-        let scoreboard = (`Computer Score: ${computerScore} \t Human Score: ${humanScore}`);
-        console.log(result);
-        console.log(scoreboard);
-        return result;
-    } else if ((humanChoice === "rock" && computerChoice === "scissors")
-    || (humanChoice === "paper" && computerChoice === "rock")
-    || (humanChoice === "scissors" && computerChoice === "paper") ){ //all cases where human wins
-        humanScore = ++humanScore;
-        let result = (`You win, ${humanChoice} beats ${computerChoice}! Human gets point!`);
-        let scoreboard = (`Computer Score: ${computerScore} \t Human Score: ${humanScore}`);
-        console.log(result);
-        console.log(scoreboard);
-        return result;
-    } else { //when there is no win or loss, we tied!
-        let result = (`${humanChoice} ties with ${computerChoice}! No one gets point!`);
-        let scoreboard = (`Computer Score: ${computerScore} \t Human Score: ${humanScore}`);
-        console.log(result);
-        console.log(scoreboard);
-        return result;
-    }
-    // Return a statement of who won that round
-    // Whoever is the winner earns a point! 
-}
-
-// Add score variables here inside this
-
-// Step 5: Write the logic to play the entire game
-// Your game will play 5 rounds. You will write a function named playGame that calls
-// playRound to play 5 rounds, keeps track of the scores and declares a winner at the end.
-
     // Add score variables here:
     // Make variable for human score
     let humanScore;
@@ -124,6 +44,62 @@ let humanSelection = getHumanChoice();
     computerScore = 0;
 
 
+// Step 4: Write the logic to play a single round
+// Your game will be played round by round. You will write a function that takes
+// the human and computer player choices as arguments, plays a single round,
+// increments the round winner’s score and logs a winner announcement.
+
+// Make variable that holds computerChoice (could use the function as the parameter)?
+// let computerSelection = getComputerChoice();
+
+// Make variable that holds humanChoice (could use function as parameter)?
+// let humanSelection = getHumanChoice();
+
+
+// Make new function that plays a round of 'Rock, Paper, Scissors', it needs two parameters
+    function playRound(computerChoice, humanChoice) {
+
+        // Make human choice variable case insensitive; type correct word, will work
+        humanChoice = humanChoice.toLowerCase();
+        //console.log(humanChoice);
+
+        // With the two choices, consider the rules of the game, who will win/lose? Can there be a tie?
+        if ((humanChoice === "rock" && computerChoice === "paper")
+        || (humanChoice === "paper" && computerChoice === "scissors")
+        || (humanChoice === "scissors" && computerChoice === "rock") ){ //all cases where computer wins
+            computerScore = ++computerScore;
+            let result = (`You lose, ${computerChoice} beats ${humanChoice}! Computer gets point!`);
+            let scoreboard = (`Computer Score: ${computerScore} \t Human Score: ${humanScore}`);
+            console.log(result);
+            console.log(scoreboard);
+            return result;
+        } else if ((humanChoice === "rock" && computerChoice === "scissors")
+        || (humanChoice === "paper" && computerChoice === "rock")
+        || (humanChoice === "scissors" && computerChoice === "paper") ){ //all cases where human wins
+            humanScore = ++humanScore;
+            let result = (`You win, ${humanChoice} beats ${computerChoice}! Human gets point!`);
+            let scoreboard = (`Computer Score: ${computerScore} \t Human Score: ${humanScore}`);
+            console.log(result);
+            console.log(scoreboard);
+            return result;
+        } else { //when there is no win or loss, we tied!
+            let result = (`${humanChoice} ties with ${computerChoice}! No one gets point!`);
+            let scoreboard = (`Computer Score: ${computerScore} \t Human Score: ${humanScore}`);
+            console.log(result);
+            console.log(scoreboard);
+            return result;
+        }
+        // Return a statement of who won that round
+        // Whoever is the winner earns a point! 
+}
+
+// Add score variables here inside this
+
+// Step 5: Write the logic to play the entire game
+// Your game will play 5 rounds. You will write a function named playGame that calls
+
+
+
 
 
 // Logic Errors / Potential Improvements
@@ -131,4 +107,6 @@ let humanSelection = getHumanChoice();
 // (-) If prompt returns non-choices, the game logic considers it a tie
 // (+) Place the case insensitive line to the getHumanChoice() istead of the playRound()
 // (?) I wonder if we can just put every function inside playGame()
-// (+) For computer choice, switch from ifelse to an array w/ random index
+// 05/10/2026 - Graphical User Interace Feature
+// (+) For computer choice, switch from ifelse use to array use
+
