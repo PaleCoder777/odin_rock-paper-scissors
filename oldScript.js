527dcf54df82e83cc28b9ff4577c92870518a4a1
PaleCoder777 <236280288+PaleCoder777@users.noreply.github.com>
2026-05-11 08:06:25 -0400

    Rename file to avoid naming confusion

---
+++ oldScript.js
@@ -0,0 +1,37 @@
+// Removed or replaced code
+
+// Step 1: Write the logic to get the computer choice
+// Declare function that gives one of three values (rock, paper, scissors) as the computer choice
+
+/* function getComputerChoiceOld() {
+    // make a variable that holds a randomn integer value from 0-100
+    let randomNumber = Math.random() * 100;
+    //console.log(randomNumber);
+    
+    // we should round the random number we get
+    let roundedNumber = Math.round(randomNumber);
+    //console.log(roundedNumber);
+
+    // make another variable to hold computer choice
+    let computerChoice;
+
+    // depending on rounded, random number, we put rock || paper || scissor in the computer choice (0-33, 67-100)
+    if (roundedNumber < 34) {
+        computerChoice = "rock";
+    } else if (roundedNumber > 66) {
+        computerChoice = "scissors";
+    } else {
+        computerChoice = "paper";
+    }
+    //console.log(computerChoice);
+
+    // in the end, we get this choice, and this will be the computer choice
+    return computerChoice;
+}
+*/
+
+/* Make human choice variable case insensitive; type correct word, will work
+humanChoice = humanChoice.toLowerCase();
+*/
+
+// HEAD
